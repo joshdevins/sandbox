77c4969407b4660c182e64cf62062bde7a9695da
Josh Devins <info@joshdevins.net>
2011-05-26 15:48:26 -0600

    Adding readme

--- asset-allocation-reporter/asset-allocation-reporter.rb
+++ asset-allocation-reporter/asset-allocation-reporter.rb
@@ -2,22 +2,29 @@
 
 class Stock
   
-  attr_reader :name
+  attr_reader :exchange
   attr_reader :ticker
+  attr_reader :name
   attr_reader :currency
-  attr_reader :marketCapInMillions
+  attr_reader :marketCap
   attr_reader :sector
   attr_reader :industry
   
-  def initialize(name, exchange, ticker, currency, marketCap, sector = "", industry = "")
-    @name = name
+  def initialize(exchange, ticker, name, currency, marketCap, sector = "", industry = "")
+    @exchange = exchange
     @ticker = ticker
+    @name = name
     @currency = currency
     @marketCap = marketCap
     @sector = sector
-    @industry = industry 
+    @industry = industry
   end
   
+  def asString
+    return "#{exchange}:#{ticker}\tname=#{name}\tcurrency=#{currency}\tmarketCap=#{marketCap}M\tsector=#{sector}\tindustry=#{industry}"
+  end
+  
+  # latest (net, gross) revenue by geography
 end
 
 class AssetAllocationReporter
@@ -26,11 +33,21 @@ class AssetAllocationReporter
     @stocks = stocks
   end
   
+  def print
+    @stocks.each do |stock|
+      puts stock.asString
+    end
+  end
+  
 end
 
 if __FILE__ == $0
   
-  aapl = Stock.new("Apple Inc.", "NASDAQ", "AAPL", "USD", 1000, "Technology", "Computer Hardware")
+  aapl = Stock.new("NASDAQ", "AAPL", "Apple Inc.", "USD", 310270, "Technology", "Computer Hardware")
+  amzn = Stock.new("NASDAQ", "AMZN", "Amazon.com, Inc.", "USD", 88150, "Services", "Retail (Catalog and Mail Order)")
   
-  currentPortfolio = AssetAllocationReporter.new([aapl])
+  currentPortfolio = AssetAllocationReporter.new([aapl, amzn])
+  
+  puts "Current portfolio:"
+  currentPortfolio.print
 end
