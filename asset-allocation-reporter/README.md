e86fe44c70d6404fa4dffb8bcf851bd13467bf56
Josh Devins <info@joshdevins.net>
2011-06-04 20:09:52 +0200

    Working...

--- asset-allocation-reporter/README.md
+++ asset-allocation-reporter/README.md
@@ -6,6 +6,7 @@ Dependencies
 References
 ---
 
- * Yahoo! Finance Exchanges: http://finance.yahoo.com/exchanges
+ * Yahoo! Finance official exchange symbols: http://finance.yahoo.com/exchanges
+ * Stock exchange codes and translations between systems (Thomson Reuters, Google Finance, Yahoo! Finance): http://www.wikinvest.com/wiki/List_of_Stock_Exchanges
  * Yahoo! Finance data download: http://www.gummy-stuff.org/Yahoo-data.htm
  * Yahoo! Finance sector and industry index: http://biz.yahoo.com/ic/ind_index.html
--- asset-allocation-reporter/asset-allocation-reporter.rb
+++ asset-allocation-reporter/asset-allocation-reporter.rb
@@ -1,27 +1,51 @@
 #!/usr/bin/env ruby
 
+require 'open-uri'
+
+class Exchange
+  
+  @@systems = {
+    thomson_reuters: "Thomson Reuters"
+    google: "Google"
+    yahoo: "Yahoo"
+  }
+  
+  attr_reader :name
+  attr_reader :symbols
+  
+  def initialize(name, symbols)
+    @name = name
+    @symbols = symbols
+  end
+  
+  def to_s
+    symbols_str = symbols.join(', ')
+    "#{name}: #{symbols_str}"
+  end
+end
+
 class Stock
   
   attr_reader :exchange
   attr_reader :ticker
   attr_reader :name
   attr_reader :currency
-  attr_reader :marketCap
+  attr_reader :market_cap
   attr_reader :sector
   attr_reader :industry
   
-  def initialize(exchange, ticker, name, currency, marketCap, sector = "", industry = "")
+  def initialize(exchange, ticker, name, currency, market_cap, sector = "", industry = "")
     @exchange = exchange
     @ticker = ticker
     @name = name
     @currency = currency
-    @marketCap = marketCap
+    @market_cap = market_cap
     @sector = sector
     @industry = industry
   end
   
-  def asString
-    return "#{exchange}:#{ticker}\tname=#{name}\tcurrency=#{currency}\tmarketCap=#{marketCap}M\tsector=#{sector}\tindustry=#{industry}"
+  def to_s
+    "#{exchange}:#{ticker}, name=#{name}, currency=#{currency}, market_cap=#{market_cap}M, sector=#{sector}, industry=#{industry}"
   end
   
   # latest (net, gross) revenue by geography
@@ -34,9 +58,7 @@ class AssetAllocationReporter
   end
   
   def print
-    @stocks.each do |stock|
-      puts stock.asString
-    end
+    @stocks.each { |stock| puts stock }
   end
   
 end
@@ -46,8 +68,8 @@ if __FILE__ == $0
   aapl = Stock.new("NASDAQ", "AAPL", "Apple Inc.", "USD", 310270, "Technology", "Computer Hardware")
   amzn = Stock.new("NASDAQ", "AMZN", "Amazon.com, Inc.", "USD", 88150, "Services", "Retail (Catalog and Mail Order)")
   
-  currentPortfolio = AssetAllocationReporter.new([aapl, amzn])
+  current_portfolio = AssetAllocationReporter.new([aapl, amzn])
   
   puts "Current portfolio:"
-  currentPortfolio.print
+  current_portfolio.print
 end
