c08a544d6e6cf79e71e9fe5cd6c72bf49bb24e8c
Josh Devins <info@joshdevins.net>
2011-06-15 21:52:44 +0200

    Fixes to bootstrap

---
+++ asset-allocation-reporter/test/test_industry_factory.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'nokogiri'
+require 'open-uri'
+
+require 'asset_allocation_reporter'
+
+class TestIndustryFactory < MiniTest::Unit::TestCase
+
+  def test_parse_yahoo_industry_index
+    
+    f = File.open("test/resources/html/finance.yahoo.com/industry_index.html")
+    html = Nokogiri::HTML(f)
+    f.close
+    
+    sectors = AssetAllocationReporter::IndustryFactory.parse_yahoo_industry_index(html)
+    asser
+  end
+  
+  def test_parse_yahoo_industry_index_live
+    html = Nokogiri::HTML(open('http://biz.yahoo.com/ic/ind_index.html'))
+    sectors = AssetAllocationReporter::IndustryFactory.parse_yahoo_industry_index(html)
+  end
+end
