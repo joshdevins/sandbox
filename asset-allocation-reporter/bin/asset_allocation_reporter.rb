fa2e2c9efc70d150774601d201faa8edddb1c3d7
Josh Devins <info@joshdevins.net>
2011-06-15 21:40:17 +0200

    File perms

--- asset-allocation-reporter/bin/asset_allocation_reporter.rb
+++ asset-allocation-reporter/bin/asset_allocation_reporter.rb
@@ -6,8 +6,8 @@ $:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
 # dependencies
 require 'asset_allocation_reporter'
 
-aapl = Stock.new("NASDAQ", "AAPL", "Apple Inc.", "USD", 310270, "Technology", "Computer Hardware")
-amzn = Stock.new("NASDAQ", "AMZN", "Amazon.com, Inc.", "USD", 88150, "Services", "Retail (Catalog and Mail Order)")
+aapl = AssetAllocationReporter::Stock.new("NASDAQ", "AAPL", "Apple Inc.", "USD", 310270, "Technology", "Computer Hardware")
+amzn = AssetAllocationReporter::Stock.new("NASDAQ", "AMZN", "Amazon.com, Inc.", "USD", 88150, "Services", "Retail (Catalog and Mail Order)")
 
 current_portfolio = AssetAllocationReporter::Portfolio.new([aapl, amzn])
 
