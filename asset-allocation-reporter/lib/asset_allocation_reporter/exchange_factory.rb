0629671b3a48daeb73c2ebd8ef86eaab5b7535f7
Josh Devins <info@joshdevins.net>
2011-06-16 15:02:03 +0200

    New factory

---
+++ asset-allocation-reporter/lib/asset_allocation_reporter/exchange_factory.rb
@@ -0,0 +1,16 @@
+module AssetAllocationReporter
+  class ExchangeFactory
+    
+    def self.parse_exchange_index():
+      
+      # parse the static file of exchange details
+      
+      
+      # full index with all symbols mapped
+      exchangeIndex = {}
+      
+      
+      return exchangeIndex
+    end
+  end
+end
