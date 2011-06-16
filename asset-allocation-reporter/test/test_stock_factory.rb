51f113dcd8fe640ae9c33cb07ee80fbb70b089fa
Josh Devins <info@joshdevins.net>
2011-06-16 21:39:10 +0200

    Fixed static test

--- asset-allocation-reporter/test/test_stock_factory.rb
+++ asset-allocation-reporter/test/test_stock_factory.rb
@@ -10,13 +10,22 @@ class TestStockFactory < MiniTest::Unit::TestCase
     html = Nokogiri::HTML(f)
     f.close
     
-    industry_html = html.xpath('/html/body/div/div[3]/table[2]/tbody/tr[2]/td/table[2]/tbody/tr/td/table/tbody/tr[3]/td[2]/a')
+    ids = parse_yahoo_profile_page(html)
+  end
+  
+  def test_parse_yahoo_profile_page_live
+    html = Nokogiri::HTML(open('http://finance.yahoo.com/q/pr?s=AAPL+Profile'))
+    ids = parse_yahoo_profile_page(html)
+  end
+  
+  def parse_yahoo_profile_page(html)
+    
+    industry_html = html.xpath('/html/body/div/div[3]/table[2]/tr[2]/td/table[2]/tr/td/table/tr[3]/td[2]/a')
     ids = AssetAllocationReporter::IndustryFactory.parse_sector_and_industry_ids_from_url(industry_html.attribute('href').value)
     
     assert_equal(8, ids.sector_id)
     assert_equal(811, ids.industry_id)
-  end
-  
-  def test_parse_yahoo_profile_page_live
+    
+    return ids
   end
 end
