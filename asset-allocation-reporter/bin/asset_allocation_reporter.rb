47a5f3a4bb44142249183e3349be1670ca19f0e0
Josh Devins <info@joshdevins.net>
2011-07-09 22:56:04 +0200

    Start with portfolios

--- asset-allocation-reporter/bin/asset_allocation_reporter.rb
+++ asset-allocation-reporter/bin/asset_allocation_reporter.rb
@@ -9,18 +9,45 @@ module AssetAllocationReporter
   
   # stocks
   lookup = [
-    LookupStock.new('AAPL',),
+    LookupStock.new('AMZN'),
+    LookupStock.new('DIS'),
     LookupStock.new('GOOG'),
-    LookupStock.new('TRI', 'TSE')]
+
+    LookupStock.new('AAPL'),
+    #LookupStock.new('BRK-B'),
+    LookupStock.new('CWB', 'TSE'),
+    #LookupStock.new('DH', 'TSE'),
+    LookupStock.new('JNJ'),
+    LookupStock.new('PG'),
+    LookupStock.new('TRI', 'TSE'),
+    ]
 
   stocks = StockFactory.lookup_stocks(lookup)
 
   # portfolios
-  current_portfolio_holdings = [
-    Holding.new(stocks[0], 10, :cad),
-    Holding.new(stocks[1], 10, :cad),
-    Holding.new(stocks[2], 100, :cad)]
-  current_portfolio = Portfolio.new("CAD$ non-reg'd", :cad, current_portfolio_holdings)
-  current_portfolio.print_with_standard_allocations()
+  itrade_non_registered_holdings = [
+    Holding.new(stocks[0], 21, :usd),
+    Holding.new(stocks[1], 39, :usd),
+    Holding.new(stocks[2], 4, :usd),
+    ]
+  itrade_non_registered = Portfolio.new("CAD$ non-registered (iTrade)", :cad, itrade_non_registered_holdings)
+  itrade_non_registered.print_with_standard_allocations()
 
+  itrade_registered_holdings = [
+    Holding.new(stocks[3], 11, :usd),
+    #Holding.new(stocks[4], 50, :usd),
+    Holding.new(stocks[4], 104, :cad),
+    #Holding.new(stocks[5], 135, :cad),
+    Holding.new(stocks[5], 34, :usd),
+    Holding.new(stocks[6], 32, :usd),
+    Holding.new(stocks[7], 64, :cad),
+    ]
+  itrade_registered = Portfolio.new("CAD$ registered (iTrade)", :cad, itrade_registered_holdings)
+  itrade_registered.print_with_standard_allocations()
+
+  # overall
+  overall = Portfolio.new("all holdings (CAD$ normalized)", :cad, [])
+  overall.merge!(itrade_non_registered)
+  overall.merge!(itrade_registered)
+  overall.print_with_standard_allocations()
 end
